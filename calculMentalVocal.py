b9290cb7e7d5eb9f52753dff900327d17c6dbb76
Mathéo TROUILLE <55890393+Warard@users.noreply.github.com>
2021-09-05 19:35:35 +0200

    Choix intervalles, opérateurs, temps réfléxion
    Ajout de la possibilité de choisir son intervalle de travail, avec quel opérateurs, et combien de temps pour réfléchir à la réponse.
    Reste à faire la gestion des erreurs dans les input, et régler le soucis de la fenêtre Pygame ne répondant pas

--- calculMentalVocal.py
+++ calculMentalVocal.py
@@ -8,28 +8,37 @@ import pygame
 
 #--------Calcul generation--------#
 # Lists of intervals
-interval1 = [1, 10]
-interval2 = [1, 100] 
+Intervals = [
+    [1, 10],
+    [1, 20],
+    [1, 30],
+    [1, 50],
+    [1, 100], 
+    [1, 1000]
+]
 
 # Liste of operators
-operators = ["+", "-", "x", "/"]
+Operators = [
+    ["+", "-", "x", "/"],
+    ["+", "-"],
+    ["x", "/"]
+]
 
-# Other
-thinkingTime = 5
-
-def randomCalcul(interval):
+def randomCalcul(interval: list, operators:list) -> str:
     '''
-    Generate a random calcul on a defined interval using the 4 operators
+    Generate a random calcul on a defined interval using the choosen operators
     Arg : 
         interval : list of two ints (begin, end) 
+        operators : list of the possible operations 
     Return : 
         question : string containing the calcul
         answer : string containing the answer of the calcul
     '''
-    # Random operation
+    # Random numbers
     n1 = random.randint(interval[0], interval[1])
     n2 = random.randint(interval[0], interval[1])
-    operator = operators[random.randint(0, 3)]
+    # Random calcul operators
+    operator = operators[random.randint(0, len(operators) -1)]
 
     # Calcul of the answer
     if operator == "+":
@@ -42,13 +51,13 @@ def randomCalcul(interval):
         result = n1 / n2
     
     # Composition of question & answer, and return
-    question = str(n1) + operator + str(n2) + "?"
+    question = str(n1) + operator + str(n2) + " = ?"
     answer = str(result)
+
     return question, answer
 
-
 #--------Voice generation--------#
-def TTS(textToSay, language = 'en'):
+def TTS(textToSay: str, language: str):
     '''
     Convert a string text into speech
     Arg : 
@@ -65,6 +74,35 @@ def TTS(textToSay, language = 'en'):
     os.system("start calcul.mp3")
 
 
+#--------Choice of game parameters--------#
+def chooseParameters():
+    '''
+    Let the player choose its interval of calcul, operators and time to think of the result
+    Arg : 
+        void
+    Return : 
+        choosenInterval : int representing the interval reading index
+        choosenOperators : int representing the operators reading index
+        thinkingTime : int representing the time to think of the result
+    '''
+    # ask the player for its game parameters
+
+    # interval choice
+    for i in range(len(Intervals)):
+        print(f'{i+1} : {Intervals[i]}')
+    choosenInterval = int(input("Intervalle choisi > "))
+
+    # operators choice
+    for i in range(len(Operators)):
+        print(f'{i+1} : {Operators[i]}')   
+    choosenOperators = int(input('Opérateurs choisi > '))
+
+    # thinking time choice 
+    thinkingTime = int(input('Combien de temps de réflexion ? > '))
+
+    return choosenInterval, choosenOperators, thinkingTime
+
+
 #--------G.U.I--------#
 backgroundColour = (255,255,255)
 (width, height) = (300, 200)
@@ -73,43 +111,52 @@ pygame.display.set_caption('Mental calcul')
 
 pygame.font.init()
 font = pygame.font.Font('Verdana.ttf', 30)
+pygame.display.flip()
 
+# Asking for parameters
+choosenInterval, choosenOperators, thinkingTime = chooseParameters()
 
 #--------Main loop--------#
 run = True
 while run:
+    pygame.display.flip()
     print("----------------------")
 
+    #----QUESTION----#
+    # Generate a calcul
+    question, answer = randomCalcul(Intervals[choosenInterval - 1], Operators[choosenOperators - 1])
+
+    # Pygame events 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    #----QUESTION----#
-    # Generate a calcul
-    question, answer = randomCalcul(interval1)
     # Read the question
-    TTS(question)
+    TTS(question, 'en')
     
     # Graphic interface update
     print(question)
-    
+
     screen.fill(backgroundColour)
     text = font.render(question, False, (0, 0, 0))
     screen.blit(text, (width//2 - 40, height//2 - 10))
     pygame.display.flip()
 
+
     #----THINKING TIME----#
     sleep(thinkingTime)
 
+
     #----ANSWER----#
     # Read the answer
-    TTS(answer)
+    TTS(answer, 'en')
     # Graphic user interface
     print(answer)
+    pygame.display.flip()
 
     screen.fill(backgroundColour)
     text = font.render(answer, False, (0, 0, 0))
     screen.blit(text, (width//2 - 20, height//2 - 10))
     pygame.display.flip()
     
-    sleep(3)
+    sleep(1)
